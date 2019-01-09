e7393e448a68523ddcf7257d12286f839cd2729f
ilyabulychev <services@ilyabulychev.com>
2019-01-09 07:56:28 +0300

    Green & Red

--- scripts/finance.page.js
+++ scripts/finance.page.js
@@ -106,7 +106,7 @@ $(document).ready(function(){
                     !CheckFinance($("#select_price").val(), price, $("#input_price").val())) {
                     $(clone).attr('class', 'ticket-trs-red');
 
-                    if($("#tickets_red").val().lengthbm > 1) {
+                    if($("#tickets_red").val().length > 1) {
                         $("#tickets_red").val($("#tickets_red").val() + ' ' + ticket.trim());
                     } else {
                         $("#tickets_red").val(ticket.trim());
